0c091d8d447a7f98761796eb0b03e7b318a2081a
Khotam <bakhromov17@gmail.com>
2023-03-27 16:41:12 +0500

    initial commit

---
+++ src/getData.ts
@@ -0,0 +1,36 @@
+import * as puppeteer from "puppeteer";
+
+const SITE_NAME = "https://islom.uz/";
+
+export const getData = async () => {
+  const browser = await puppeteer.launch({ timeout: 0 });
+  const page = await browser.newPage();
+  await page.goto(SITE_NAME, {
+    waitUntil: "domcontentloaded",
+  });
+
+  const data: Record<string, string[]> = await page.evaluate(() => {
+    const prayertimeEls = [...document.querySelectorAll(".p_clock > b")];
+    const titleEls = [...document.querySelectorAll(".in_cricle > div.p_v")];
+
+    const prayerTimes = prayertimeEls.map((el) => el.innerHTML);
+    const titles = titleEls.map((el) => el.innerHTML);
+
+    return { titles, prayerTimes };
+  });
+
+  await browser.close();
+
+  const values = Object.values(data);
+
+  const result = values[0].reduce(
+    (acc: Record<string, string>, curr: string, idx) => {
+      acc[curr] = values[1][idx];
+      return acc;
+    },
+    {}
+  );
+
+  console.log("result", result);
+  return result;
+};
